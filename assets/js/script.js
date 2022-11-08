ab5a4da999f3e29c2bb97951e524061d4aec1aad
BluSkreen <jasonduran303@gmail.com>
2022-11-07 22:38:09 -0700

    added save event function that stores the event text as an object in local storage. working on function to add local storage to textarea element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var ;
 
+// Hour cards
+// var am9El = $("#hour-9");
+// var am10El = $("#hour-10");
+// var am11El = $("#hour-11");
+// var am12El = $("#hour-12");
+// var pm1El = $("#hour-13");
+// var pm2El = $("#hour-14");
+// var pm3El = $("#hour-15");
+// var pm4El = $("#hour-16");
+// var pm5El = $("#hour-17");
+
+var buttons = $(".saveBtn");
+// var events = [];
+
+// card colors: past, present, future
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -11,16 +25,41 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  buttons.on("click", saveEvent);
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  $(".row").each(function () {
+    if() {
+      
+    }
+    var lastEvent = JSON.parse(localStorage.getItem($(this).attr("id")));
+    console.log(lastEvent.eventID);
+  });
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var saveEvent = function (event) {
+  event.preventDefault();
+  // get id and text
+  var id = $(this).parent().attr("id");
+  var text = $(this).parent().children("textarea").val();
+  // make object
+  var event = {
+    eventID: id,
+    eventText: text,
+  };
+  // push object to events[]
+  // events.push(event);
+  // local storage
+  localStorage.setItem(id, JSON.stringify(event));
+};
