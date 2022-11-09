9eccf30b8ed38c3be76e5835c8336a6d56bc532e
BluSkreen <jasonduran303@gmail.com>
2022-11-08 19:44:10 -0700

    the display in header, with the current date, is finished. index.html/script.js have descriptive comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// Hour cards
-// var am9El = $("#hour-9");
-// var am10El = $("#hour-10");
-// var am11El = $("#hour-11");
-// var am12El = $("#hour-12");
-// var pm1El = $("#hour-13");
-// var pm2El = $("#hour-14");
-// var pm3El = $("#hour-15");
-// var pm4El = $("#hour-16");
-// var pm5El = $("#hour-17");
-
+// pointer to saveBtn class
 var buttons = $(".saveBtn");
-var events = [];
-
-// card colors: past, present, future
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // listener that add text from the time-block to local storage
   buttons.on("click", saveEvent);
-
   // Determin the color of the block relative to the current hour
   $(".time-block").each(function () {
     // gets the hour of the block using its id -> .each(element.ID)
@@ -34,7 +11,6 @@ $(function () {
     var currentHour = dayjs().hour();
     // compare current hour to the block and color accordingly
     // remove any color classes befor adding one
-
     $(this).removeClass("past");
     $(this).removeClass("present");
     $(this).removeClass("future");
@@ -54,12 +30,20 @@ $(function () {
     if (localStorage.getItem(blockID)) {
       var localText = localStorage.getItem(blockID);
       $(this).children().eq(1).text(localText);
-      // console.log(localStorage.getItem(blockID));
+      console.log(localStorage.getItem(blockID));
     }
-    // console.log($(this).attr("id"));
+    console.log($(this).attr("id"));
   });
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // Display the current date in the header of the page.
+  $("#currentDay").text(
+    "The current day is: " +
+      dayjs().year() +
+      "/" +
+      dayjs().month() +
+      "/" +
+      dayjs().day()
+  );
 });
 
 var saveEvent = function (event) {
